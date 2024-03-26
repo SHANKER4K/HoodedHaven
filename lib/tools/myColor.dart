933b206e3dbcc14b9f90f9f6b020b0f0499cb129
SHANKER <medjahdiismail1998@gmail.com>
2024-03-26 09:05:15 +0100

    Revert "thirdcolor"
    This reverts commit 538ab7ebe8aab13527e3f25aac10dd16a24edd2c.

--- lib/tools/myColor.dart
+++ lib/tools/myColor.dart
@@ -3,6 +3,4 @@ import 'package:flutter/material.dart';
 class myColors {
   static Color primaryColor = Colors.white;
   static Color secoundColor = Color.fromARGB(255, 202, 201, 201);
-  static Color thirdcolor = Colors.black38;
-  int? shk;
 }
