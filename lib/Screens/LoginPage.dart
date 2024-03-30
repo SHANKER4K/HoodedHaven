881df00fb8e8ec5d11f899b140eeacc5a9574306
ismailferdi <135427088+ismailferdi@users.noreply.github.com>
2024-03-30 14:39:07 +0100

    Update LoginPage.dart

--- lib/Screens/LoginPage.dart
+++ lib/Screens/LoginPage.dart
@@ -1,107 +1,160 @@
+import 'dart:ui';
 import 'package:flutter/material.dart';
-import 'package:hoodedhaven/Screens/RegisterPage.dart';
-
-import '../tools/ResposiveSize.dart';
+import 'package:hoodedhaven/pages/RegisterPage.dart';
+import 'package:hoodedhaven/tools/responsivesize.dart';
 
 class LoginPage extends StatelessWidget {
-  const LoginPage({super.key});
+   const LoginPage({super.key});
 
   @override
   Widget build(BuildContext context) {
     // Define a function to convert a design size to a responsive size
-
+    Size size = MediaQuery.of(context).size;
     return Scaffold(
       extendBodyBehindAppBar: true,
       appBar: AppBar(
-        backgroundColor: Color.fromARGB(0, 255, 255, 255),
+        backgroundColor: const Color.fromARGB(0, 255, 255, 255),
         elevation: 0.0,
       ),
       body: Container(
         width: double.infinity, // Remove fixed width
         height: double.infinity, // Remove fixed height
-        decoration: BoxDecoration(
+        decoration: const BoxDecoration(
           image: DecorationImage(
-            image: AssetImage("assets/Login.jpg"),
+            image: AssetImage("assets/tester.jpg"),
+          
             fit: BoxFit.cover,
           ),
         ),
-        child: SingleChildScrollView(
-          // Allow scrolling on smaller screens
-          child: Column(
-            children: [
-              Container(
-                height: ResponsiveSize.responsiveSize(context, 210),
-                width: ResponsiveSize.responsiveSize(context, 350),
-                margin: EdgeInsets.fromLTRB(
-                    0, ResponsiveSize.responsiveSize(context, 424), 0, 0),
-                decoration: BoxDecoration(
-                  color: Color.fromARGB(255, 242, 239, 224),
-                  border: Border.all(color: Colors.black, width: 2.0),
-                ),
-                child: Column(
-                  children: [
-                    Container(
-                        padding: EdgeInsets.fromLTRB(14, 16, 13, 0),
-                        child: SizedBox(
-                          height: 40,
-                          child: TextField(
-                            decoration: InputDecoration(
-                                border: InputBorder.none,
-                                hintText: "Email",
-                                hintStyle: TextStyle(
-                                    fontSize: 13, fontWeight: FontWeight.w600),
-                                enabledBorder: OutlineInputBorder(
-                                    borderSide: BorderSide(
-                                        color: Colors.black, width: 2))),
-                          ),
-                        )),
-                    Container(
-                        padding: EdgeInsets.fromLTRB(14, 2, 13, 0),
-                        child: SizedBox(
-                          height: 40,
-                          child: TextField(
-                            scribbleEnabled: false,
-                            maxLines: 1,
-                            decoration: InputDecoration(
-                                hintStyle: TextStyle(
-                                    fontSize: 13, fontWeight: FontWeight.w600),
-                                hintText: "Password",
-                                enabledBorder: OutlineInputBorder(
-                                    borderSide: BorderSide(
-                                        color: Colors.black, width: 2))),
-                          ),
-                        )),
-                    SizedBox(
-                      height: 5,
+        child: BackdropFilter(
+          filter: ImageFilter.blur(sigmaX: 1.3,sigmaY: 1.3),
+          child: Center(
+            child: SingleChildScrollView(
+              // Allow scrolling on smaller screens
+              child: Column(
+                children: [
+                  
+                  Container(
+                    height: ResponsiveSize.responsiveSize(context, 600),
+                    width: ResponsiveSize.responsiveSize(context, 500),
+                    margin: const EdgeInsets.only(top:60 ,),
+                    padding: const EdgeInsets.only(top: 70,bottom: 10,),
+                    decoration: BoxDecoration(
+                      color: const Color.fromRGBO(255, 247, 228, 0),
+                      border: Border.all(color: const Color.fromRGBO(255, 247, 228, 0), width: 2.0),
+                      
                     ),
-                    MaterialButton(
-                      onPressed: () {
-                        Navigator.pushNamed(context, "/bottomnavbar");
-                      },
-                      minWidth: 250,
-                      height: 50,
-                      color: Color.fromARGB(255, 20, 60, 72),
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(33)),
-                      child: Text("LOGIN"),
+                    child: Column(
+                      children: [
+                        
+                         const Text(
+                              'SIGN IN',
+                              style: TextStyle(
+                                fontSize: 25,
+                                fontWeight: FontWeight.w600,
+                                color:  Color.fromRGBO(229, 186, 143, 8),
+                              ),
+                        ),
+                        const SizedBox(height: 18,),              
+                        Container(
+                        
+                          height: size.width / 10,
+                          width: size.width / 1.65,
+                          alignment: Alignment.center,
+                          padding: EdgeInsets.only(right: size.width / 30),
+                          decoration: BoxDecoration(
+                            color: Colors.black.withOpacity(.1),
+                            borderRadius: BorderRadius.circular(20),
+                            
+                          ),
+                          child: TextField(
+                            style: TextStyle(
+                            color: Colors.white.withOpacity(.9),
+                            ),
+            
+                          keyboardType: TextInputType.emailAddress,
+                          decoration: InputDecoration(
+                          prefixIcon: Icon(
+                            Icons.email_outlined,
+                            color: Colors.white.withOpacity(.8),
+                          ),
+                          border: InputBorder.none,
+                          hintMaxLines: 1,
+                          hintText: "Email",
+                          hintStyle: TextStyle(
+                          fontSize: 14,
+                          color: Colors.white.withOpacity(.8),
+                          ),
+                          ),
+                          ),
+                          ),
+                          const SizedBox(height: 7,),
+                          //password
+                        Container(
+                          height: size.width / 10,
+                          width: size.width / 1.65,
+                          alignment: Alignment.center,
+                          padding: EdgeInsets.only(right: size.width / 30),
+                          decoration: BoxDecoration(
+                            color: Colors.black.withOpacity(.1),
+                            borderRadius: BorderRadius.circular(20),
+                          ),
+                          child: TextField(
+                            style: TextStyle(
+                            color: Colors.white.withOpacity(.9),
+                            ),
+            
+                          keyboardType: TextInputType.text,
+                          decoration: InputDecoration(
+                          prefixIcon: Icon(
+                            Icons.lock_outline,
+                            color: Colors.white.withOpacity(.8),
+                          ),
+                          border: InputBorder.none,
+                          hintMaxLines: 1,
+                          hintText: "Password",
+                          hintStyle: TextStyle(
+                          fontSize: 14,
+                          color: Colors.white.withOpacity(.8),
+                          ),
+                          ),
+                          ),
+                          ),
+                        const SizedBox(
+                          height: 123,
+                        ),
+                        //minWidth: 250,
+                          //height: 50,
+                        MaterialButton(
+                    onPressed: () {
+                      Navigator.pushNamed(context, "/bottomnavbar");
+                    },
+                    minWidth: 250,
+                    height: 50,
+                    color: const Color.fromRGBO(229, 186, 143, 8),
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(33)),
+                    child: const Text("Login"),
+                  ),
+                        TextButton(
+                          onPressed: () {
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => const RegisterPage(),
+                                ));
+                          },
+                          child: const Text(
+                            "Create a New Account  ",
+                            style: TextStyle(color: Colors.white54),
+                          ),
+                        )
+                      ],
                     ),
-                    TextButton(
-                      onPressed: () {
-                        Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) => RegisterPage(),
-                            ));
-                      },
-                      child: Text(
-                        "Register...",
-                        style: TextStyle(color: Colors.black),
-                      ),
-                    )
-                  ],
-                ),
+                  ),
+                ],
               ),
-            ],
+            ),
           ),
         ),
       ),
